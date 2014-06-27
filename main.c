ba22c98ff1d441a79c06637fed094acaddf4f85f
Sebastiao Ferreira <sebastorama@gmail.com>
2014-06-27 00:21:15 -0300

    Fixed indentation

--- main.c
+++ main.c
@@ -9,43 +9,43 @@
 #include <stdbool.h>
 
 void print_usage() {
-	printf("Usage: equalizer <file>\n Where file is a windows bmp file");
+  printf("Usage: equalizer <file>\n Where file is a windows bmp file");
 }
 
 void draw_image(BMP_pixel_matrix bmp, SDL_Surface * s, int x, int y) {
-	int i,j;
-	for(i = (int)bmp.height-1; i >= 0; i--) {
-		for(j = 0; j < (int)bmp.width; j++) {
-			pixelRGBA(s, x+j, y+ ((int)bmp.height-1-i),
-					bmp.pixels[i][j].red,
-					bmp.pixels[i][j].green,
-					bmp.pixels[i][j].blue,
-					0xFF);
-		}
-	}
-	SDL_Flip(s);
+  int i,j;
+  for(i = (int)bmp.height-1; i >= 0; i--) {
+    for(j = 0; j < (int)bmp.width; j++) {
+      pixelRGBA(s, x+j, y+ ((int)bmp.height-1-i),
+          bmp.pixels[i][j].red,
+          bmp.pixels[i][j].green,
+          bmp.pixels[i][j].blue,
+          0xFF);
+    }
+  }
+  SDL_Flip(s);
 }
 
 void draw_histogram(SDL_Surface * s, unsigned long int * histogram, int depth, int scale, int x, int y, Uint32 color) {
-	int i, j;
-	unsigned long int * scaled_histogram = calloc(depth, sizeof(unsigned long int));
-	unsigned long int max_hist = 0;
+  int i, j;
+  unsigned long int * scaled_histogram = calloc(depth, sizeof(unsigned long int));
+  unsigned long int max_hist = 0;
 
-	for(i = 0; i < depth; i++)
-		if(histogram[i] > max_hist) max_hist = histogram[i];
+  for(i = 0; i < depth; i++)
+    if(histogram[i] > max_hist) max_hist = histogram[i];
 
-	for(i = 0; i < depth; i++) {
-		scaled_histogram[i] = histogram[i] * scale / max_hist;
-		vlineColor(s, x+i, y+scale, y+scale-scaled_histogram[i], color);
-	}
+  for(i = 0; i < depth; i++) {
+    scaled_histogram[i] = histogram[i] * scale / max_hist;
+    vlineColor(s, x+i, y+scale, y+scale-scaled_histogram[i], color);
+  }
 
-	free(scaled_histogram);
-	SDL_Flip(s);
+  free(scaled_histogram);
+  SDL_Flip(s);
 }
 
 
 void update_histogram(unsigned long int color, unsigned long int * histogram) {
-	histogram[color]++;
+  histogram[color]++;
 }
 
 void calculate_cdf(unsigned long int * histogram, unsigned long int * cdf, int depth) {
@@ -72,83 +72,83 @@ void equalize_pixel(unsigned long int * pixel, unsigned long int * cdf, unsigned
 }
 
 int main(int argc, char ** argv) {
-	SDL_Surface *screen;
+  SDL_Surface *screen;
 
-	if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
-	{
-		SDL_Quit();
-		return 1;
-	}
+  if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
+  {
+    SDL_Quit();
+    return 1;
+  }
 
-	int i, j;
+  int i, j;
 
-	unsigned long int * r_hist =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * g_hist =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * b_hist =
-		calloc(256, sizeof(unsigned long int));
+  unsigned long int * r_hist =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * g_hist =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * b_hist =
+    calloc(256, sizeof(unsigned long int));
 
-	unsigned long int * r_hist_eq =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * g_hist_eq =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * b_hist_eq =
-		calloc(256, sizeof(unsigned long int));
+  unsigned long int * r_hist_eq =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * g_hist_eq =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * b_hist_eq =
+    calloc(256, sizeof(unsigned long int));
 
-	unsigned long int * cdf_r_hist =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * cdf_g_hist =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * cdf_b_hist =
-		calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_r_hist =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_g_hist =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_b_hist =
+    calloc(256, sizeof(unsigned long int));
 
-	unsigned long int * cdf_r_hist_eq =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * cdf_g_hist_eq =
-		calloc(256, sizeof(unsigned long int));
-	unsigned long int * cdf_b_hist_eq =
-		calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_r_hist_eq =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_g_hist_eq =
+    calloc(256, sizeof(unsigned long int));
+  unsigned long int * cdf_b_hist_eq =
+    calloc(256, sizeof(unsigned long int));
 
-	unsigned long int r_scale, g_scale, b_scale;
-	unsigned long int r_min, g_min, b_min;
+  unsigned long int r_scale, g_scale, b_scale;
+  unsigned long int r_min, g_min, b_min;
 
-	FILE * input_file;
-	FILE * output_file;
+  FILE * input_file;
+  FILE * output_file;
 
-	input_file = fopen(argv[1], "rb");
+  input_file = fopen(argv[1], "rb");
 
-	if(argc < 2) {
-		print_usage();
-		exit(0);
-	}
+  if(argc < 2) {
+    print_usage();
+    exit(0);
+  }
 
 
-	bmp_header header = get_header(input_file);
-	BMP_pixel_matrix bmp_matrix = get_pixels(input_file);
+  bmp_header header = get_header(input_file);
+  BMP_pixel_matrix bmp_matrix = get_pixels(input_file);
 
-	draw_image(bmp_matrix, screen, 0, 0);
+  draw_image(bmp_matrix, screen, 0, 0);
 
-	for(i = 0; i < (int)bmp_matrix.height; i++) {
-		for(j = 0; j < (int)bmp_matrix.width; j++) {
-			update_histogram(bmp_matrix.pixels[i][j].red, r_hist);
-			update_histogram(bmp_matrix.pixels[i][j].green, g_hist);
-			update_histogram(bmp_matrix.pixels[i][j].blue, b_hist);
-		}
-	}
+  for(i = 0; i < (int)bmp_matrix.height; i++) {
+    for(j = 0; j < (int)bmp_matrix.width; j++) {
+      update_histogram(bmp_matrix.pixels[i][j].red, r_hist);
+      update_histogram(bmp_matrix.pixels[i][j].green, g_hist);
+      update_histogram(bmp_matrix.pixels[i][j].blue, b_hist);
+    }
+  }
 
 
   calculate_cdf(r_hist, cdf_r_hist, 256);
   calculate_cdf(g_hist, cdf_g_hist, 256);
   calculate_cdf(b_hist, cdf_b_hist, 256);
 
-	draw_histogram(screen, r_hist, 256, 128, 0, bmp_matrix.height+((128+10)*0), 0xFF0000FF);
-	draw_histogram(screen, g_hist, 256, 128, 0, bmp_matrix.height+((128+10)*1), 0x00FF00FF);
-	draw_histogram(screen, b_hist, 256, 128, 0, bmp_matrix.height+((128+10)*2), 0x0000FFFF);
+  draw_histogram(screen, r_hist, 256, 128, 0, bmp_matrix.height+((128+10)*0), 0xFF0000FF);
+  draw_histogram(screen, g_hist, 256, 128, 0, bmp_matrix.height+((128+10)*1), 0x00FF00FF);
+  draw_histogram(screen, b_hist, 256, 128, 0, bmp_matrix.height+((128+10)*2), 0x0000FFFF);
 
-	draw_histogram(screen, cdf_r_hist, 256, 128, 0, bmp_matrix.height+((128+10)*0), 0xFFFFFF88);
-	draw_histogram(screen, cdf_g_hist, 256, 128, 0, bmp_matrix.height+((128+10)*1), 0xFFFFFF88);
-	draw_histogram(screen, cdf_b_hist, 256, 128, 0, bmp_matrix.height+((128+10)*2), 0xFFFFFF88);
+  draw_histogram(screen, cdf_r_hist, 256, 128, 0, bmp_matrix.height+((128+10)*0), 0xFFFFFF88);
+  draw_histogram(screen, cdf_g_hist, 256, 128, 0, bmp_matrix.height+((128+10)*1), 0xFFFFFF88);
+  draw_histogram(screen, cdf_b_hist, 256, 128, 0, bmp_matrix.height+((128+10)*2), 0xFFFFFF88);
 
   r_min = first_non_zero(r_hist, 256);
   g_min = first_non_zero(g_hist, 256);
@@ -156,8 +156,8 @@ int main(int argc, char ** argv) {
 
   printf("%lu %lu %lu/n", r_min, g_min, b_min);
 
-	for(i = 0; i < (int)bmp_matrix.height; i++) {
-		for(j = 0; j < (int)bmp_matrix.width; j++) {
+  for(i = 0; i < (int)bmp_matrix.height; i++) {
+    for(j = 0; j < (int)bmp_matrix.width; j++) {
       r_scale = (unsigned long int) bmp_matrix.pixels[i][j].red;
       g_scale = (unsigned long int) bmp_matrix.pixels[i][j].green;
       b_scale = (unsigned long int) bmp_matrix.pixels[i][j].blue;
@@ -180,30 +180,30 @@ int main(int argc, char ** argv) {
   calculate_cdf(g_hist_eq, cdf_g_hist_eq, 256);
   calculate_cdf(b_hist_eq, cdf_b_hist_eq, 256);
 
-	draw_histogram(screen, r_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*0), 0xFF0000FF);
-	draw_histogram(screen, g_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*1), 0x00FF00FF);
-	draw_histogram(screen, b_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*2), 0x0000FFFF);
+  draw_histogram(screen, r_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*0), 0xFF0000FF);
+  draw_histogram(screen, g_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*1), 0x00FF00FF);
+  draw_histogram(screen, b_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*2), 0x0000FFFF);
 
-	draw_histogram(screen, cdf_r_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*0), 0xFFFFFF88);
-	draw_histogram(screen, cdf_g_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*1), 0xFFFFFF88);
-	draw_histogram(screen, cdf_b_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*2), 0xFFFFFF88);
+  draw_histogram(screen, cdf_r_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*0), 0xFFFFFF88);
+  draw_histogram(screen, cdf_g_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*1), 0xFFFFFF88);
+  draw_histogram(screen, cdf_b_hist_eq, 256, 128, bmp_matrix.width+10, bmp_matrix.height+((128+10)*2), 0xFFFFFF88);
 
-	output_file = fopen("results.bmp", "w+b");
-	write_header(header, output_file);
-	write_pixels(header, bmp_matrix, output_file);
+  output_file = fopen("results.bmp", "w+b");
+  write_header(header, output_file);
+  write_pixels(header, bmp_matrix, output_file);
 
-	draw_image(bmp_matrix, screen, bmp_matrix.width+20, 0);
+  draw_image(bmp_matrix, screen, bmp_matrix.width+20, 0);
 
-	fclose(output_file);
-	fclose(input_file);
+  fclose(output_file);
+  fclose(input_file);
 
-	SDL_Event event;
-	bool running = true;
-	while(running) {
-		while (SDL_PollEvent(&event) && event.key.keysym.sym == SDLK_q) {
-			if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN)
-				running = false;
-		}
-	}
-	return 0;
+  SDL_Event event;
+  bool running = true;
+  while(running) {
+    while (SDL_PollEvent(&event) && event.key.keysym.sym == SDLK_q) {
+      if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN)
+        running = false;
+    }
+  }
+  return 0;
 }
